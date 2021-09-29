95f530e5f44a6679665943225adace108ae46010
John <mccano@protonmail.com>
2021-09-29 05:51:02 -0700

    added tinymce for product edit

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -24,7 +24,18 @@
                                 <h3 class="card-title">Create New Product</h3>
                             </div>
                             <!-- /.card-header -->
-                            <!-- form start -->
+                            @if(count($errors) > 0 )
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                    <ul class="p-0 m-0" style="list-style: none;">
+                                        @foreach($errors->all() as $error)
+                                            <li>{{$error}}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
                             <form action="{{route("updateProduct",$product->id)}}" method="POST" enctype="multipart/form-data">
                                 @csrf
                                 <div class="card-body">
@@ -32,6 +43,11 @@
                                         <label >Name</label>
                                         <input type="text" class="form-control" name="name"
                                                placeholder="Enter Name" value="{{$product->name}}">
+                                    </div>
+                                    <div class="form-group">
+                                        <label >Description</label>
+                                        <textarea class="form-control description" name="description"
+                                                  placeholder="Enter Description" value="">{!! $product->description !!}</textarea>
                                     </div>
                                     <div class="form-group">
                                         <label >Price</label>
@@ -67,6 +83,24 @@
 @endsection
 
 @section("scripts")
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script type="text/javascript">
+        tinymce.init({
+            selector:'textarea.description',
+            height: 300,
+            menubar: true,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+                'bold italic backcolor | alignleft aligncenter ' +
+                'alignright alignjustify | bullist numlist outdent indent | ' +
+                'removeformat | help',
+            content_css: '//www.tiny.cloud/css/codepen.min.css'
+        });
+    </script>
     <script>
         function previewImage(input){
             var file = $("input[type=file]").get(0).files[0];
