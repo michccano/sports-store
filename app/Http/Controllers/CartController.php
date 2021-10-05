41b27c171796863096d8dfafc44a4e6c49935f7f
John <mccano@protonmail.com>
2021-10-05 07:30:31 -0700

    prevent auth user to access login and registration page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\User;
 use Gloudemans\Shoppingcart\Facades\Cart;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
@@ -47,20 +44,4 @@ class CartController extends Controller
         return response()->json(['cartCount' => $cartCount , 'hasToken' => $hasToken]);
     }
 
-    public function checkout(){
-        $payment = Cart::total();
-
-        $user = User::with('purchaseToken','bonusToken','makeupToken')->find(Auth::id());
-        $userToken = $user->token;
-        $tokenLeft = $userToken - $payment;
-        if($tokenLeft >=0){
-            Cart::destroy();
-            $user->token = $tokenLeft;
-            $user->save();
-            return redirect()->route('shop')->with("successMessage","Order Placed Successfully");
-        }
-        else{
-            return redirect()->route('cart.show')->with("errorMessage","Don't Have Enough Token");
-        }
-    }
 }
