3d054ad9a9a7786e8dee81f7d2ace80670442e06
John <mccano@protonmail.com>
2021-10-12 19:27:09 -0700

    update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -29,6 +29,7 @@ class CreateUsersTable extends Migration
             $table->string('dayphone')->nullable()->unique();
             $table->string('evephone')->nullable()->unique();
             $table->integer('status')->default(1);
+            $table->integer('reg_verify')->default(0);
             $table->integer('role')->default(2);
             $table->integer('pick')->default(0);
             $table->integer("token")->nullable();
