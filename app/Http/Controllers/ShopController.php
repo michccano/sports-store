90efe091d575f29e5c625ff324e390d21cbcd925
John <mccano@protonmail.com>
2021-10-18 01:20:05 -0700

    inactive product product after expire date

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function index(){
+        $expired_products = Product::where('expire_date', '<=', Carbon::now())->get();
+        foreach ($expired_products as $product){
+            $product->status = 0;
+            $product->save();
+        }
         $products = Product::where("status",1)->get();
         $cart = Cart::content();
         return view('shop.shop',compact("products","cart"));
