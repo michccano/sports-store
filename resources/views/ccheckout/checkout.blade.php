abb574cb73a09ca115048053ede42656e3cd551a
John <mccano@protonmail.com>
2021-11-11 16:47:58 +0600

    update

--- resources/views/ccheckout/checkout.blade.php
+++ resources/views/ccheckout/checkout.blade.php
@@ -3,108 +3,128 @@
 @section('title', 'Payment')
 
 @section('content')
-    <section class="shop-category-main-area">
-        <div class="container">
-            <div class="shop-category-inner-wrap">
-                <div class="shop-category-title">
-                    <h3>Pay From Card</h3>
-                </div>
+<section class="shop-category-main-area">
+    <div class="container">
+        <div class="shop-category-inner-wrap">
+            <div class="shop-category-title">
+                <h3>Pay From Card</h3>
             </div>
         </div>
-    </section>
-    <section class="shop-product-main-area">
-        <div class="container">
-            <div class="shop-product-inner-wrap">
-                <div class="shop-product-title">
+    </div>
+</section>
+<section class="shop-product-main-area">
+    <div class="container">
+        <div class="shop-product-inner-wrap">
+            <div class="shop-product-title checkout-page">
 
-                    <form action="{{ route('charge') }}" method="post">
-                        @csrf
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">First Name</label>
-                                    <input type="text" class="form-control" name="firstname"
-                                           placeholder="Enter First Name" value="{{$user->firstname}}">
+                <form action="{{ route('charge') }}" method="post">
+
+                    @csrf
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="product-details-area">
+                                <h3 class="mb-4">Product Details</h3>
+                                <div class="product-details-content-image">
+                                    <img src="" alt="Product Image">
+                                </div>
+                                <div class="product-details-content">
+                                    <div class="p_name">Product Name</div>
+                                    <div class="p_details"> jnzkcvccvxjn</div>
+                                    <div class="p_name">Product Price</div>
+                                    <div class="p_details"> $100</div>
+                                    <div class="p_name">Product Name</div>
+                                    <div class="p_details"> jnzkcvccvxjn</div>
+                                    <div class="p_name">Product Name</div>
+                                    <div class="p_details"> jnzkcvccvxjn</div>
+                                    <div class="p_name">Product Name</div>
+                                    <div class="p_details"> jnzkcvccvxjn</div>
+                                    <div class="p_name">Product Name</div>
+                                    <div class="p_details"> jnzkcvccvxjn</div>
+                                    <div class="p_name">Product Name</div>
+                                    <div class="p_details"> jnzkcvccvxjn</div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Last Name</label>
-                                    <input type="text" class="form-control" name="lastname"
-                                           placeholder="Enter Last Name" value="{{$user->lastname}}">
+
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">First Name</label>
+                                        <input type="text" class="form-control" name="firstname" placeholder="Enter First Name" value="{{$user->firstname}}">
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Address</label>
-                            <input type="text" class="form-control" name="address"
-                                   placeholder="Enter Address" value="{{$user->address1}}">
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">City</label>
-                                    <input type="text" class="form-control" name="city"
-                                           placeholder="Enter City" value="{{$user->city}}">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Last Name</label>
+                                        <input type="text" class="form-control" name="lastname" placeholder="Enter Last Name" value="{{$user->lastname}}">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Post Code</label>
-                                    <input type="text" class="form-control" name="postal"
-                                           placeholder="Enter Post Code" value="{{$user->postal}}">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Address</label>
+                                        <input type="text" class="form-control" name="address" placeholder="Enter Address" value="{{$user->address1}}">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">State</label>
-                                    <input type="text" class="form-control" name="state"
-                                           placeholder="Enter State" value="{{$user->state}}">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">City</label>
+                                        <input type="text" class="form-control" name="city" placeholder="Enter City" value="{{$user->city}}">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-3">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Country</label>
-                                    <input type="text" class="form-control" name="country"
-                                           placeholder="Enter Country" value="{{$user->country}}">
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Post Code</label>
+                                        <input type="text" class="form-control" name="postal" placeholder="Enter Post Code" value="{{$user->postal}}">
+                                    </div>
                                 </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">State</label>
+                                        <input type="text" class="form-control" name="state" placeholder="Enter State" value="{{$user->state}}">
+                                    </div>
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Country</label>
+                                        <input type="text" class="form-control" name="country" placeholder="Enter Country" value="{{$user->country}}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Phone Number</label>
+                                        <input type="text" class="form-control" name="phone" placeholder="Enter Phone Number" value="{{$user->dayphone}}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Card Number</label>
+                                        <input type="text" class="form-control" name="cc_number" placeholder="Enter Card Number" />
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="exampleInputEmail1">Expire Month</label>
+                                    <input type="text" class="form-control" name="expiry_month" placeholder="Enter Month" />
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="exampleInputEmail1">Expire Year</label>
+                                    <input type="text" class="form-control" name="expiry_year" placeholder="Enter Year" />
+                                </div>
+                                <div class="col-md-4">
+                                    <label for="exampleInputEmail1">Card CVV</label>
+                                    <input type="text" class="form-control" name="cvv" placeholder="CVV" />
+                                </div>
+                                <div class="col-md-12 mt-3">
+                                    <button type="submit" class="btn btn-primary form-checkout-submit-btn">Submit</button>
+                                </div>
+
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Phone Number</label>
-                            <input type="text" class="form-control" name="phone"
-                                   placeholder="Enter Phone Number" value="{{$user->dayphone}}">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Card Number</label>
-                            <input type="text" class="form-control"  name="cc_number" placeholder="Enter Card Number" />
-                        </div>
-
-                        <div class="row">
-                            <div class="col-md-4">
-                                <label for="exampleInputEmail1">Expire Month</label>
-                                <input type="text" class="form-control"  name="expiry_month" placeholder="Enter Month" />
-                            </div>
-                            <div class="col-md-4">
-                                <label for="exampleInputEmail1">Expire Year</label>
-                                <input type="text" class="form-control"  name="expiry_year" placeholder="Enter Year" />
-                            </div>
-                            <div class="col-md-4">
-                                <label for="exampleInputEmail1">Card CVV</label>
-                                <input type="text" class="form-control"  name="cvv" placeholder="CVV" />
-                            </div>
-                        </div>
-
-                        <br>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+                </form>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
-
